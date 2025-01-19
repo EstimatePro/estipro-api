ecf9ac90dedec20e81d000407bd9da09c5bb2706
Viacheslav Melnichenko <melnichenkoviatcheslav@gmail.com>
2025-01-19 22:28:25 +0100

    update config

--- src/EstiPro.Application/Options/Auth0Options.cs
+++ src/EstiPro.Application/Options/Auth0Options.cs
@@ -3,11 +3,17 @@ namespace EstiPro.Application.Options;
 public sealed class Auth0Options
 {
     public const string SectionName = "Auth0";
-    public string Audience { get; set; } = null!;
-    public string ClientId { get; set; } = null!;
     public string Connection { get; set; } = null!;
     public string Domain { get; set; } = null!;
     public string PureDomain { get; set; } = null!;
-    public string GrantType { get; set; } = null!;
+    public Auth0Api EstiPro { get; set; } = null!;
+    public Auth0Api EstiProApi { get; set; } = null!;
+}
+
+public sealed class Auth0Api
+{
+    public string Audience { get; set; } = null!;
+    public string ClientId { get; set; } = null!;
     public string ClientSecret { get; set; } = null!;
+    public string GrantType { get; set; } = null!;
 }
